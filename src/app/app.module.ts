abf42453ad3207b17029766f30e90f5da60eb520
Luke King <lkingfrm@gmail.com>
2019-04-29 01:12:36 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HerosComponent } from './heros/heros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HerosComponent
   ],
   imports: [
     BrowserModule,
