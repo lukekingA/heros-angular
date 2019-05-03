9a5beb28169c3b005d4a8e645451d49510ff1fb2
Luke King <lkingfrm@gmail.com>
2019-05-02 23:55:51 -0600

    added the get function in the heros service with the http method

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 import { Hero } from './hero';
@@ -8,14 +10,37 @@ import { HEROS } from './mock-heros';
   providedIn: 'root'
 })
 export class HeroService {
+  private herosUrl = 'api/heros';
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // todo: send the srror to a remote logging ifrastructure
+      console.log(error);
+      // todo: better job of transforming the error fot the user
+      this.log(`${operation} failed: ${error.message}`);
+      // let the app keep running by returning an empty reault
+      return of(result as T);
+    };
+  }
+
   getHeros(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heros');
-    return of(HEROS);
+    return this.http.get<Hero[]>(this.herosUrl).pipe(
+      tap(_ => this.log('fetched heros')),
+      catchError(this.handleError<Hero[]>('getHeros', []))
+    );
   }
   getHero(id: number): Observable<Hero> {
-    this.messageService.add('HeroService: fetched one hero');
+    this.log('fetched one hero');
     const hero: Hero = HEROS.filter(h => h.id === id)[0];
+    console.log(of(hero));
     return of(hero);
   }
-  constructor(private messageService: MessageService) {}
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {}
 }
